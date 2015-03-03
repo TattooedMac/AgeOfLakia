ba57d0d9e560b4c6f3634d8627dccddfbd6dba46
Brent Robertson <tattooedmac01@me.com>
2015-03-03 19:40:37 +0800

    first rev

--- AgeOFLakia/ViewController.swift
+++ AgeOFLakia/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelDogYears: UILabel!
+    @IBOutlet weak var convertedDogYearsText: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,42 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func convertToDogYears(sender: UIButton) {
+        
+        let stringFromTextField = convertedDogYearsText.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        let intFromOptional = optionalIntFromTextField!
+        
+        labelDogYears.text = "\(intFromOptional * 7)" + " Human Years"
+        
+        labelDogYears.hidden = false
+        convertedDogYearsText.resignFirstResponder()
+        
+    }
+    
+    @IBAction func convertAgeRequirement(sender: UIButton) {
+        
+        let stringFromTextField = convertedDogYearsText.text
+        let optionalDoubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+        var realDogYears:Double
+        //I have added new inputs to convert to Real Human years. 
+        //Each human year equals 10.5 dog years for the first 2 years then 4 yrs for every 
+        //year after that. 
+
+        if optionalDoubleFromTextField > 2
+        {
+            realDogYears = (10.5 * 2) + (optionalDoubleFromTextField - 2) * 4
+        }
+        else
+        {
+            realDogYears = optionalDoubleFromTextField * 10.5
+        }
+        
+        labelDogYears.text = "\(realDogYears)" + " Real Human Years"
+        convertedDogYearsText.resignFirstResponder()
     }
 
 
